b99a0c23a6bb2a361ee1641bb4d6466fa301231d
HonvedoAdam <honvedoadamkj@gmail.com>
2022-01-22 09:12:24 +0100

    toggle navbar with burger

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 //https://www.youtube.com/watch?v=-D6oTPA4vXc&t=30s 53:58
+
+const haamburgerButton = document.getElementById("hamburger");
+const navlist = document.getElementById("navlist");
+
+function toggleButton() {
+  navlist.classList.toggle("show");
+}
+
+haamburgerButton.addEventListener("click", toggleButton)
